a7257821b6d7f2cd33ddbe52c7d0a3ec94cf4609
Utente <Utente@DESKTOP-JO50B91>
2023-11-29 19:44:29 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp7/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Studente
+    { 
+        public class Persona
+            {  
+                public string Name { get; set; }
+                public string Cognome { get; set; }
+                public DateTime DataDiNascita { get; set; }
+            } 
+        public class Insegnanti 
+            {  
+            public float Voto { get; set;}
+            public string Materia { get; set;}
+            public DateTime Data { get; set; }
+            }
+    }
+    internal class Insegnante
+    {
+        public class Persona
+        {
+            public string Name { get; set; }
+            public string Cognome { get; set; }
+            public DateTime DataDiNascita { get; set; }
+        }
+        public class Materia
+        {
+            public float Sigla { get; set; }
+            public string Nome { get; set; }
+        }
+    }
+
+}
+
